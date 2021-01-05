6b72e66250c4f18adf4b710c87c0a51cb8a96600
bcExpt1123 <bcExpt1123@gmail.com>
2021-01-05 17:28:53 +0900

    Changed Workout Video modal

--- src/app/pages/home/sections/Workout/ModalView.js
+++ src/app/pages/home/sections/Workout/ModalView.js
@@ -1,22 +1,23 @@
 import React, { useState, useEffect } from "react";
 import {  useDispatch, useSelector } from "react-redux";
 import classnames from "classnames";
-import { Button, Modal } from "react-bootstrap";
+import { Modal } from "react-bootstrap";
 import ModalBlock from "./ModalBlock";
 import VideoView from "./VideoView";
 import CompleteView from "./CompleteView";
 import SectionNote from '../../DashboardPage/SectionNote';
-import Icon from "../../components/Icon";
-import { convertVideo,setVideo } from "../../redux/workout/actions";
+import {isMobile} from '../../../../../_metronic/utils/utils';
+import { convertVideo,convertContent, setVideo } from "../../redux/workout/actions";
 import { stopRunning } from "../../redux/done/actions";
 
 const ModalView = ({ isOpen, onClose }) => {
   const dispatch = useDispatch();
   const workouts = useSelector(({done})=>done.workouts);
-  const [instructionShow,setInstructionShow] = useState(false);
   const step = useSelector(({workout})=>workout.step);
   const view = useSelector(({workout})=>workout.view);
   const video = useSelector(({workout})=>workout.video);
+  const originalVideo = useSelector(({workout})=>workout.originalVideo);
+  const modalVideo = useSelector(({workout})=>workout.modalVideo);
   const isRunning = useSelector(({done})=>done.isRunning);
   const renderLine = (line,index)=>{
     switch(line.tag){
@@ -59,9 +60,12 @@ const ModalView = ({ isOpen, onClose }) => {
     onClose();
     return true;
   }
-  const onInstructionHide = ()=>{
-    setInstructionShow(false);
-  }
+  useEffect(()=>{
+    if(!isMobile() && view!=='video') {
+      const dialog = document.getElementsByClassName("modal-dialog")[0];
+      if(dialog)dialog.style.width = "auto";
+    }
+  },[view]);  
   return (
     <>
       <Modal
@@ -70,6 +74,7 @@ const ModalView = ({ isOpen, onClose }) => {
         animation={false}
         className={classnames({"workouts-modal":true, video:(view==='video' || view==='instruction'), trans:(view==='video')})}
         centered
+        dialogClassName={classnames({"video-dialog":(view==='video')})}
         size="lg"
         backdrop="static"
       >
@@ -92,10 +97,18 @@ const ModalView = ({ isOpen, onClose }) => {
                 </button>               */}
                 <Modal.Title className="w-100"> 
                   {video.name}
+                  {originalVideo.id == video.id&&
+                    <div className={"active"}>
+                      Activo
+                    </div>
+                  }
                 </Modal.Title>
-                <button type="button" className={"back-button help"} onClick={() => { setInstructionShow(true);}}>
+                <button type="button" className="close" onClick={() => { if(modalVideo)dispatch(convertContent());  else onClose();}}>
+                  <span aria-hidden="true">×</span><span className="sr-only">Close</span>
+                </button>                
+                {/* <button type="button" className={"back-button help"} onClick={() => { setInstructionShow(true);}}>
                   <i className="fas fa-book"/>
-                </button>              
+                </button>               */}
               </>
             )}
             {/* {view==='instruction'&&(
@@ -138,22 +151,6 @@ const ModalView = ({ isOpen, onClose }) => {
           }
         </Modal.Body>
       </Modal>
-      <Modal
-        show={instructionShow}
-        onHide={onInstructionHide}
-        animation={false}
-        className={classnames("instruction-modal")}
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title className="w-100 mt-2">
-            Instrucciones
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {video.instruction}
-        </Modal.Body>
-      </Modal>      
     </>
   );
 }
